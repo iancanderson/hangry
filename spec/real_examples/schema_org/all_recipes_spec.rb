8e88d46e39902254091172f0ec3035127b398f51
Chris TenHarmsel <chris@tenharmsel.com>
2016-02-15 16:42:34 -0600

    Fixing AllRecipes.com recipe parsing
    The latest version of the AllRecipes.com website does not require the
    specialized parser this commit removes it.  Updated the captured HTML
    from allrecipes.com and updated the spec to match its new information.

--- spec/real_examples/schema_org/all_recipes_spec.rb
+++ spec/real_examples/schema_org/all_recipes_spec.rb
@@ -1,4 +1,5 @@
 require 'hangry'
+require 'rspec/its'
 
 describe Hangry do
 
@@ -6,15 +7,11 @@ describe Hangry do
     let(:html) { File.read("spec/fixtures/schema_org/allrecipes.html") }
     subject { Hangry.parse(html) }
 
-    it "should use the correct parser" do
-      expect(Hangry::ParserClassSelecter.new(html).parser_class).to eq(Hangry::Parsers::NonStandard::AllRecipesParser)
-    end
-    
-    its(:author) { should == "United Soybean Board" }
-    its(:canonical_url) { should == "http://allrecipes.com/recipe/roasted-vegetable-and-couscous-salad/" }
+    its(:author) { should == 'United Soybean Board' }
+    its(:canonical_url) { should == 'http://allrecipes.com/recipe/230347/roasted-vegetable-and-couscous-salad/'}
     its(:cook_time) { should == 15 }
-    its(:description) { should == "\"This better-for-you main-dish salad is quick, colorful and full of satisfying texture. To explore a variety of grains, substitute 3 cups cooked regular couscous, brown rice or quinoa.\"" }
-    its(:image_url) { should == "http://images.media-allrecipes.com/userphotos/250x250/00/98/77/987785.jpg" }
+    its(:description) { should == '"This better-for-you main-dish salad is quick, colorful and full of satisfying texture. To explore a variety of grains, substitute 3 cups cooked regular couscous, brown rice or quinoa."' }
+    its(:image_url) { should == 'http://images.media-allrecipes.com/userphotos/250x250/987785.jpg' }
     its(:ingredients) {
       should == [
         '3 cups broccoli florets, cut into 1/2-inch pieces',
@@ -30,15 +27,15 @@ describe Hangry do
     its(:name) { should == "Roasted Vegetable and Couscous Salad" }
     its(:nutrition) do
       should == {
-        calories: '305',
-        cholesterol: '0mg',
-        fiber: '4.3g',
-        protein: '6.8g',
-        saturated_fat: '2.2g',
-        sodium: '217mg',
-        sugar: '4.1g',
+        calories: '305 kcal',
+        cholesterol: '0 mg',
+        fiber: nil,
+        protein: '6.8 g',
+        saturated_fat: nil,
+        sodium: '217 mg',
+        sugar: nil,
         total_carbohydrates: '38.3g',
-        total_fat: '14.2g',
+        total_fat: '14.2 g',
         trans_fat: nil,
         unsaturated_fat: nil
       }
@@ -57,11 +54,8 @@ You may substitute 3 cups cooked regular couscous, brown rice or quinoa.
     its(:prep_time) { should == 15 }
     its(:published_date) { should == nil }
     its(:total_time) { should == 30 }
-    its(:yield) { should == "6 servings" }
+    its(:yield) { should == '6' }
 
   end
 
 end
-
-
-
